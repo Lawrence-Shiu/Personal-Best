3dbe800e0b11830d63e06ef8ce526e09cb598d82
Lawrence <lawshiu123@gmail.com>
2019-02-05 20:32:45 -0800

    second

---
+++ app/src/main/java/com/example/lawsh/personalbest/fitness/FitnessService.java
@@ -0,0 +1,8 @@
+package com.example.lawsh.personalbest.fitness;
+
+public interface FitnessService {
+    int getRequestCode();
+    void setup();
+    void updateStepCount();
+    //long getSteps();
+}

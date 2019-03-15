b860741507db4aef487e601d66b023e45178c694
Tim Corvese <tcorvese@ucsd.edu>
2019-03-15 01:00:08 -0700

    note trigger

--- functions/index.js
+++ functions/index.js
@@ -26,6 +26,7 @@ exports.sendChatNotifications = functions.firestore
          notification: {
            title: document.from + ' sent you a message',
            body: document.text
+           click_action: 'OPEN_ACTIVITY_NOTE'
          },
          topic: context.params.chatId
        };
